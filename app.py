89fc6814130888e3bbf39e1d1d0b96548703d748
Turtlezz <ov@turtlezz.dk>
2024-11-23 23:11:22 +0100

    made the initial app

---
+++ app.py
@@ -0,0 +1,48 @@
+import random
+from itertools import repeat
+
+import nltk
+
+nltk.download('words')
+
+
+def get_random_word():
+    word_list = [word for word in nltk.corpus.words.words() if len(word) == 5]
+    random_word = random.choice(word_list)
+
+    word_length = str(len(random_word))
+
+    print("Welcome to the app!")
+    print("The length of the word is " + word_length)
+
+    guesscount = 1
+    while guesscount < 5:
+        guess = input(f"Take your {guesscount} guess!\n -> ")
+        if guess == random_word:
+            print("You guessed it right!")
+            win = True
+            guesscount = 5
+            break
+        else:
+            feedback = []
+            for i in range(len(guess)):
+                if i < len(random_word) and guess[i] == random_word[i]:
+                    feedback.append(guess[i])
+                elif guess[i] in random_word:
+                    feedback.append(f"({guess[i]})")
+                else:
+                    feedback.append('_')
+            print("Not quite right. Here are the correct characters so far: " + ''.join(feedback))
+            guesscount += 1
+            print("Number of guesses:", guesscount)
+            if guesscount == 5:
+                print("You lost! The word was:", random_word)
+                win = False
+
+
+while True:
+    get_random_word()
+    play_again = input("Do you want to play again? (yes/no)\n -> ")
+    if play_again.lower() != 'yes':
+        print("Goodbye!")
+        break
