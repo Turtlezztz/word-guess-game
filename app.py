d04a83b45eddce95a330a8a57c2bd4c67868e6a8
Turtlezz <ov@turtlezz.dk>
2024-11-24 00:54:12 +0100

    big changes to game so it only uses nouns but it's really slow

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 import random
+from collections import Counter
+
+
 import nltk
 
-print("Downloading the words corpus from nltk...")
+print("Fetching the words corpus from nltk...")
 nltk.download('words', quiet=True)
+nltk.download('brown', quiet=True)
+nltk.download('averaged_perceptron_tagger_eng', quiet=True)
+brown_words = nltk.corpus.brown.words()
+word_freq = Counter(brown_words)
+all_words = nltk.corpus.words.words()
+common_words = [word for word, _ in word_freq.most_common() if word in all_words]
+
+# Filtering the words to only include nouns
+tagged_words = nltk.pos_tag(common_words)
+nouns = [word for word, pos in tagged_words if pos.startswith == 'NN']
+
+common_nouns = [word for word, _ in word_freq.most_common() if word in nouns]
 print("Done!")
 
 
-def get_random_word():
-    word_list = [word for word in nltk.corpus.words.words() if len(word) == 5]
-    random_word = random.choice(word_list)
+def word_game():
+    print("Choose a difficulty level: "
+          "\n1. Easy (100 most common nouns)"
+          "\n2. Medium (10000 most common nouns)"
+          "\n3. Hard (Almsot every noun in the English language)")
+    difficulty = input("Enter the number of the difficulty level: ")
+    if difficulty == '1':
+        word_selection = common_nouns[:100]
+    elif difficulty == '2':
+        word_selection = common_nouns[:10000]
+    else:
+        word_selection = common_nouns
+
+    random_word = random.choice(word_selection)
 
     word_length = str(len(random_word))
 
@@ -46,7 +72,7 @@ def get_random_word():
 
 
 while True:
-    get_random_word()
+    word_game()
     play_again = input("Do you want to play again? (yes/no)\n -> ")
     if play_again.lower() != 'yes':
         print("Goodbye!")
